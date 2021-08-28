e330f8a801a4b680298b68de683b7e42d72278e7
OsamaBinNaughty-hub <driesvdb25@gmail.com>
2021-08-28 04:59:25 +0200

    MultiSelect & DropdownMultiSelect

--- hippo_v2/lib/controller/landing_page_controller.dart
+++ hippo_v2/lib/controller/landing_page_controller.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hippo_v2/view/widget/landing_widgets/multiselect.dart';
 
 class LandingController extends ChangeNotifier{
   // call this function if you want to go to MainPage
   late Function(String) onAuthorization;
+
+
 
   TextEditingController firstNameController = new TextEditingController();
   TextEditingController lastNameController = new TextEditingController();
@@ -13,5 +17,4 @@ class LandingController extends ChangeNotifier{
     lastNameController.dispose();
     super.dispose();
   }
-
 }
--- hippo_v2/lib/view/page/landing_page.dart
+++ hippo_v2/lib/view/page/landing_page.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hippo_v2/controller/landing_page_controller.dart';
 import 'package:hippo_v2/view/widget/base_view.dart';
+import 'package:hippo_v2/view/widget/landing_widgets/multiselect.dart';
 import 'package:hippo_v2/view/widget/landing_widgets/landing_textfield.dart';
 import 'package:hippo_v2/view/widget/landing_widgets/landing_title.dart';
+
+//TODO: Dependable dropdowns made from previous choice in DropdownList() & DropdownMultiDialog()
+//TODO: Put DropdownList() & DropdownMultiDialog() in own file in landing_widgets dir
+/** IDEAS:
+ * (https://stackoverflow.com/questions/39645641/best-way-to-implement-no-password-sign-in-with-firebase)
+ * First name / Last name / vub mail adress / (maybe) student ID
+ * Press next button //TODO: design next button
+ * --> onPressed: TODO: no-signIn/signUp auth to firebase (https://stackoverflow.com/questions/39645641/best-way-to-implement-no-password-sign-in-with-firebase)
+ * Always "logged in" with the TOKEN (while having user information on back-end side)
+ * --> Why? : Problems to specific use
+**/
 
 class Landing extends StatefulWidget {
   const Landing({ Key? key }) : super(key: key);
@@ -24,6 +36,7 @@ class _LandingState extends State<Landing> {
         ),
       );
     };
+
     return BaseView<LandingController>(
       onControllerReady: (controller){
         controller.onAuthorization = onAuthorization;
@@ -90,8 +103,10 @@ class _LandingState extends State<Landing> {
                               labelText: 'What course are you following?',
                             ),
                             SizedBox(height: 3*8,),
-                            DropdownList(
-                              labelText: 'What group are you in?',
+                            //DropdownList(labelText: 'What group are you in? (More than 1 possible)',),
+                            //SizedBox(height: 3*8,),
+                            DropdownMultiSelectDialog(
+                              labelText: "What group are you in? (More than 1 possible)",
                             ),
                           ],
                         ),
@@ -113,6 +128,7 @@ class DropdownList extends StatefulWidget {
   DropdownList({
     required this.labelText
   });
+
   @override
   _DropdownListState createState() => _DropdownListState();
 }
@@ -150,5 +166,72 @@ class _DropdownListState extends State<DropdownList> {
         ),
       ),
     );
+  }
+}
+
+class DropdownMultiSelectDialog extends StatefulWidget {
+  const DropdownMultiSelectDialog({Key? key,
+    required this.labelText,
+  }) : super(key: key);
+  final String labelText;
+  @override
+  _DropdownMultiSelectDialogState createState() => _DropdownMultiSelectDialogState();
+}
+
+class _DropdownMultiSelectDialogState extends State<DropdownMultiSelectDialog> {
+  String dropdownValue='Nothing Selected';
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ()=>showGroupMultiselect(context),
+      child: InputDecorator(
+        decoration: InputDecoration(
+          labelText: widget.labelText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+        ),
+        child: Container(
+          height: 20,
+          child: Row(
+            children: [
+              Text(
+                dropdownValue,
+                style: TextStyle(
+                  fontSize: 16
+                ),
+              ),
+              SizedBox(width: 158,),//SCUFFED AS FUCK MAAR IK GEEF GEEN FUCK...
+              Icon(
+                Icons.arrow_drop_down,
+                color: Colors.grey.shade700,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //TODO: automatic showGroupMultiSelect.
+  //TODO: CANCEL/SUBMIT BUTTONS NOT WORKING ? ALTHOUGH IT WORKED BEFORE!!!! BULLSHIT
+  void showGroupMultiselect(BuildContext context) async {
+    final items = <MultiSelectDialogItem<int>>[
+      MultiSelectDialogItem(1, 'Group 1'),
+      MultiSelectDialogItem(2, 'Group 2'),
+      MultiSelectDialogItem(3, 'What the fuck is this bullshit code?'),
+    ];
+
+    final selectedValues = await showDialog<Set<int>>(
+      context: context,
+      builder: (BuildContext context) {
+        return MultiSelectDialog(
+          items: items,
+          initialSelectedValues: [].toSet(),
+        );
+      },
+    );
+    print(selectedValues);
   }
 }
